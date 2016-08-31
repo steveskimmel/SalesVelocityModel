ec84800096c9a9c7f235e89cd429588a8c27e898
steveskimmel <steveskimmel@gmail.com>
2016-08-31 13:55:33 +0200

    return number not json

--- ws.py
+++ ws.py
@@ -1,5 +1,5 @@
 from salesModel import salesModel
-from flask import Flask, request, jsonify
+from flask import Flask, request
 from flask_restful import Resource, Api
 import ast
 
@@ -14,16 +14,16 @@ class webService(Resource):
     def post(self):
         if 'calc_sales' in request.form:
             variables_dict = ast.literal_eval(request.form['calc_sales'])
-            return jsonify(calculatedSales=SalesVelocityModel.calc_sales_per_year(variables_dict['deals'],variables_dict['deal_size'],variables_dict['win_rate'],variables_dict['avg_sales_cycle']))
+            return SalesVelocityModel.calc_sales_per_year(variables_dict['deals'],variables_dict['deal_size'],variables_dict['win_rate'],variables_dict['avg_sales_cycle'])
         elif 'sales_perc_increase' in request.form:
             variables_dict = ast.literal_eval(request.form['sales_perc_increase'])
-            return jsonify(salesPercentageIncrease=SalesVelocityModel.new_sales_percentage_increase(variables_dict['new_sales']))
+            return SalesVelocityModel.new_sales_percentage_increase(variables_dict['new_sales'])
         elif 'variables_perc_increase' in request.form:
             variables_dict = ast.literal_eval(request.form['variables_perc_increase'])
-            return jsonify(variablesPercentageIncrease=SalesVelocityModel.uniform_variable_percentage_increase(variables_dict['percentage_increase']))
+            return SalesVelocityModel.uniform_variable_percentage_increase(variables_dict['percentage_increase'])
         elif 'increased_sales_value' in request.form:
             variables_dict = ast.literal_eval(request.form['increased_sales_value'])
-            return jsonify(newSalesValue=SalesVelocityModel.new_sales_value(variables_dict['percentage_increase_deals'],variables_dict['percentage_increase_deal_size'],variables_dict['percentage_increase_win_rate'],variables_dict['percentage_increase_avg_sales_cycle']))
+            return SalesVelocityModel.new_sales_value(variables_dict['percentage_increase_deals'],variables_dict['percentage_increase_deal_size'],variables_dict['percentage_increase_win_rate'],variables_dict['percentage_increase_avg_sales_cycle'])
 
 api.add_resource(webService, '/')
 

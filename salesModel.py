762c5cd354cf4c59331b965db22ea62cf79e46e7
steveskimmel <steveskimmel@gmail.com>
2016-09-07 17:52:17 +0200

    fix

--- salesModel.py
+++ salesModel.py
@@ -42,7 +42,7 @@ class salesModel():
 
         return round(np.real(variable_percentage_increase)[0],2)
 
-    def new_sales_value(self,percentage_increase_deals, percentage_increase_deal_size, percentage_increase_win_rate, percentage_increase_avg_sales_cycle,current_deals,current_deal_size,current_win_rate,current_avg_sales_cycle):
+    def new_sales_value(self,percentage_increase_deals,percentage_increase_deal_size,percentage_increase_win_rate,percentage_increase_avg_sales_cycle,current_deals,current_deal_size,current_win_rate,current_avg_sales_cycle):
         percentage_increase_deals += 0.0
         percentage_increase_deal_size += 0.0
         percentage_increase_win_rate += 0.0
@@ -56,8 +56,6 @@ class salesModel():
         sales_value = (((1+percentage_increase_deals)*current_deals)*((1+percentage_increase_deal_size)*current_deal_size)*((1+percentage_increase_win_rate)*(current_win_rate/100)))/((1-percentage_increase_avg_sales_cycle)*(current_avg_sales_cycle/365))
         return int(round(sales_value))
 
-    def new_sales_value_test(self,a,b,c,d,e,f,g,h):
-        return 10
 
 #current = calc_sales_per_year(deals, deal_size, win_rate, avg_sales_cycle)
 #perc = increase(new_sales)
